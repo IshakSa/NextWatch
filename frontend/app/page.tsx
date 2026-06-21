c670cea6bf1aac282f12abe1e6d9322d2a965ca0
IshakSa <s.ishak2310@gmail.com>
2026-06-21 23:27:06 +0200

    feat: update HomePage to use refactored ContentCarousel version
    - Update HomePage component to modern ContentCarousel implementation
    - Remove nested <Section> tag from HeroContentCarousel for better page-level section layout

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,15 +6,33 @@ export default async function Home() {
   const content = movieData.slice(0, 5);
   return (
     <main>
-      <HeroContentCarousel content={content} />
+      <section>
+        <HeroContentCarousel content={content} />
+      </section>
 
       <section className="container">
-        <ContentCarousel carouselType="poster" rowName="Just Release" />
+        <ContentCarousel
+          carouselType="poster"
+          rowName="Just Release"
+          content={movieData}
+        />
 
-        <ContentCarousel carouselType="ranked" rowName="Popular" />
+        <ContentCarousel
+          carouselType="ranked"
+          rowName="Popular"
+          content={movieData}
+        />
 
-        <ContentCarousel carouselType="backdrop" rowName="Movies" />
-        <ContentCarousel carouselType="backdrop" rowName="Series" />
+        <ContentCarousel
+          carouselType="backdrop"
+          rowName="Movies"
+          content={movieData}
+        />
+        <ContentCarousel
+          carouselType="backdrop"
+          rowName="Series"
+          content={movieData}
+        />
       </section>
     </main>
   );
