454e2ba4499520a39125a00a41bf2c683fff2780
IshakSa <s.ishak2310@gmail.com>
2026-06-24 19:00:51 +0200

    refactor: add support for episodes in ContentCarousel and update component structure
    - ContentCarousel classnames for different carouselTypes defined in "CONTENT_STYLES" and "ITEM_STYLES"
    - Updated `EpisodesCarousel` to utilize `ContentCarousel` for episode display and `SelectButton`.

--- frontend/components/carousel/ContentCarousel/ContentCarousel.tsx
+++ frontend/components/carousel/ContentCarousel/ContentCarousel.tsx
@@ -6,29 +6,57 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 
-import { Actor, ContentItem, Director } from "@/lib/constants";
+import { Actor, ContentItem, Director, Episode } from "@/lib/constants";
 import PosterContentCard from "./_components/PosterContentCard";
 import BackdropContentCard from "./_components/BackdropContentCard";
 import RankedContentCard from "./_components/RankedContentCard";
 import CreditsCard from "./_components/CreditsCard";
+import EpisodeCard from "./_components/EpisodeCard";
 
-type ContentCarouselProps<
-  T extends "poster" | "backdrop" | "ranked" | "credits",
-> = {
+type AllowedCarouselTypes =
+  | "poster"
+  | "backdrop"
+  | "ranked"
+  | "credits"
+  | "episodes";
+
+type ContentCarouselProps<T extends AllowedCarouselTypes> = {
   rowName: string;
   carouselType: T;
-  content: T extends "credits" ? Actor[] | Director[] : ContentItem[];
+  content: T extends "credits"
+    ? Actor[] | Director[]
+    : T extends "episodes"
+      ? Episode[]
+      : ContentItem[];
   margin?: string;
 };
 
-export default function ContentCarousel<
-  T extends "poster" | "backdrop" | "ranked" | "credits",
->({
+const CONTENT_STYLES = {
+  poster: "px-30 -ml-35",
+  backdrop: "px-30 -ml-35",
+  ranked: "sm:px-50 sm:-ml-55 md:px-5 md:-ml-10 lg:px-30 lg:-ml-35",
+  credits: "sm:px-30 sm:-ml-35 md:px-0 md:-ml-4",
+  episodes: "px-20 -ml-25 sm:px-30 sm:-ml-35",
+};
+
+const ITEM_STYLES = {
+  poster: "basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/4",
+  backdrop: "basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/4",
+  ranked: "basis-full md:basis-1/2 xl:basis-1/3",
+  credits:
+    "basis-full basis-1/2 sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5 2xl:basis-1/6",
+  episodes: "basis-full md:basis-1/2 xl:basis-1/3",
+};
+
+export default function ContentCarousel<T extends AllowedCarouselTypes>({
   rowName,
   carouselType,
   content,
   margin = "mt-25",
 }: ContentCarouselProps<T>) {
+  const contentClassName = CONTENT_STYLES[carouselType];
+  const itemClassName = ITEM_STYLES[carouselType];
+
   return (
     <div className={`${margin}`}>
       <h2 className="mb-5">{rowName}</h2>
@@ -38,28 +66,10 @@ export default function ContentCarousel<
         }}
         className="group relative"
       >
-        <CarouselContent
-          className={
-            carouselType === "ranked"
-              ? "sm:px-50 sm:-ml-55 md:px-5 md:-ml-10 lg:px-30 lg:-ml-35"
-              : carouselType === "credits"
-                ? "sm:px-30 sm:-ml-35 md:px-0 md:-ml-4"
-                : "px-30 -ml-35"
-          }
-        >
+        <CarouselContent className={contentClassName}>
           {content.map((item, index) => {
             return (
-              <CarouselItem
-                key={index}
-                className={`pl-5 basis-full 
-                  ${
-                    carouselType === "ranked"
-                      ? "md:basis-1/2 xl:basis-1/3"
-                      : carouselType === "credits"
-                        ? "basis-1/2 sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5 2xl:basis-1/6"
-                        : "sm:basis-1/2 md:basis-1/3 lg:basis-1/4"
-                  }`}
-              >
+              <CarouselItem key={index} className={`pl-5 ${itemClassName}`}>
                 {carouselType === "poster" ? (
                   <PosterContentCard contentItem={item as ContentItem} />
                 ) : carouselType === "backdrop" ? (
@@ -70,8 +80,10 @@ export default function ContentCarousel<
                     contentItem={item as ContentItem}
                     rank={index + 1}
                   />
-                ) : (
+                ) : carouselType === "credits" ? (
                   <CreditsCard person={item as Actor | Director} />
+                ) : (
+                  <EpisodeCard episode={item as Episode} />
                 )}
               </CarouselItem>
             );
---
+++ frontend/components/carousel/EpisodesCarousel/_components/SelectButton.tsx
@@ -0,0 +1,38 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export default function SelectButton({
+  selectedSeason,
+  handleSeasonChange,
+  seasonsAmount,
+}: {
+  selectedSeason: number;
+  handleSeasonChange: (newSelectedSeason: string | null) => void;
+  seasonsAmount: number;
+}) {
+  return (
+    <Select
+      value={`Season ${selectedSeason}`}
+      onValueChange={handleSeasonChange}
+    >
+      <SelectTrigger className="w-45">
+        <SelectValue placeholder="Select Watch Option" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          {Array.from({ length: seasonsAmount }, (_, index) => (
+            <SelectItem key={index} value={`Season ${index + 1}`}>
+              Season {index + 1}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
