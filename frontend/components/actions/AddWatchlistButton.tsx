048fc5345f78f881936757a56f70a05ab92a824c
IshakSa <s.ishak2310@gmail.com>
2026-06-25 14:54:13 +0200

    feat: add Toast Notifications for AddWatchlist button

--- frontend/components/actions/AddWatchlistButton.tsx
+++ frontend/components/actions/AddWatchlistButton.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { BookmarkIcon } from "lucide-react";
+import { BookmarkCheck, BookmarkIcon } from "lucide-react";
 import { Button } from "../ui/button";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { useState } from "react";
+import { toast } from "sonner";
 
 export default function AddWatchlistButton({
   className,
@@ -15,8 +16,34 @@ export default function AddWatchlistButton({
   const [isSaved, setIsSaved] = useState(false);
   const isDesktop = useMediaQuery("(min-width: 640px)");
 
+  const removedToast = () => {
+    toast.success("Removed from watchlist");
+  };
+
+  const addedToast = (previousState: boolean) => {
+    toast("Added to watchlist", {
+      description: "Saved to your list successfully.",
+      icon: <BookmarkCheck className="h-5 w-5 text-primary" />,
+      action: {
+        label: "Undo",
+        onClick: () => {
+          removedToast();
+          setIsSaved(previousState);
+        },
+      },
+    });
+  };
+
   function handleAddWatchlist() {
-    setIsSaved(!isSaved);
+    const previousState = isSaved;
+    const newState = !isSaved;
+
+    setIsSaved(newState);
+    if (newState) {
+      addedToast(previousState);
+    } else {
+      removedToast();
+    }
   }
 
   return (
@@ -27,7 +54,7 @@ export default function AddWatchlistButton({
       onClick={handleAddWatchlist}
     >
       <div className="flex space-x-2 items-center">
-        <BookmarkIcon fill={isSaved ? "#ffffff" : "none"} />
+        <BookmarkIcon fill={isSaved ? "var(--foreground)" : "none"} />
         {((isDesktop && page === "details") || page === "home") && (
           <p>{isSaved ? "Added to Watchlist" : "Add Watchlist"}</p>
         )}
