2ae1574bd8c5d46434cc67683c0968975fc96bbd
IshakSa <s.ishak2310@gmail.com>
2026-06-27 17:58:31 +0200

    feat: add data fetching from backend for home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,37 +1,104 @@
 import ContentCarousel from "@/components/carousel/ContentCarousel/ContentCarousel";
 import HeroContentCarousel from "@/components/hero/HeroContentCarousel";
-import { movieData } from "@/lib/constants";
+import { ContentItem } from "@/lib/constants";
+
+async function getLatest() {
+  const response = await fetch(`${process.env.BACKEND_URL}/api/content/latest`);
+  if (!response.ok) {
+    throw new Error("failed to fetch data");
+  }
+
+  return await response.json();
+}
+
+async function getHero() {
+  const response = await fetch(
+    `${process.env.BACKEND_URL}/api/content/trending/day`,
+  );
+  if (!response.ok) {
+    throw new Error("failed to fetch data");
+  }
+
+  return await response.json();
+}
+
+async function getRanked() {
+  const response = await fetch(
+    `${process.env.BACKEND_URL}/api/content/trending/week`,
+  );
+  if (!response.ok) {
+    throw new Error("failed to fetch data");
+  }
+
+  return await response.json();
+}
+
+async function getMovies() {
+  const response = await fetch(
+    `${process.env.BACKEND_URL}/api/content/movie/top-rated`,
+  );
+  if (!response.ok) {
+    throw new Error("failed to fetch data");
+  }
+
+  return await response.json();
+}
+
+async function getSeries() {
+  const response = await fetch(
+    `${process.env.BACKEND_URL}/api/content/tv/top-rated`,
+  );
+  if (!response.ok) {
+    throw new Error("failed to fetch data");
+  }
+
+  return await response.json();
+}
 
 export default async function Home() {
-  const content = movieData.slice(0, 5);
+  const latestData = getLatest();
+  const heroData = getHero();
+  const rankedData = getRanked();
+  const moviesData = getMovies();
+  const seriesData = getSeries();
+
+  const [latest, hero, ranked, movies, series]: ContentItem[][] =
+    await Promise.all([
+      latestData,
+      heroData,
+      rankedData,
+      moviesData,
+      seriesData,
+    ]);
+
   return (
     <main>
       <section>
-        <HeroContentCarousel content={content} />
+        <HeroContentCarousel content={hero} />
       </section>
 
       <section className="container">
         <ContentCarousel
           carouselType="poster"
           rowName="Just Release"
-          content={movieData}
+          content={latest}
         />
 
         <ContentCarousel
           carouselType="ranked"
           rowName="Popular"
-          content={movieData}
+          content={ranked}
         />
 
         <ContentCarousel
           carouselType="backdrop"
           rowName="Movies"
-          content={movieData}
+          content={movies}
         />
         <ContentCarousel
           carouselType="backdrop"
           rowName="Series"
-          content={movieData}
+          content={series}
         />
       </section>
     </main>
