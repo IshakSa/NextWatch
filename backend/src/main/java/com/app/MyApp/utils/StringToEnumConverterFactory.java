773d3006cd34c83d66d4d3b25b7de6b5e94a8c53
IshakSa <s.ishak2310@gmail.com>
2026-06-27 16:05:11 +0200

    feat: register StringToEnumConverterFactory for global URL-to-enum conversion
    - this allows us to pass normal path variables and request params like "/movie" instead of the uppercase enum value "/MOVIES"

---
+++ backend/src/main/java/com/app/MyApp/utils/StringToEnumConverterFactory.java
@@ -0,0 +1,33 @@
+package com.app.MyApp.utils;
+
+import org.springframework.core.convert.converter.Converter;
+
+import org.springframework.core.convert.converter.ConverterFactory;
+
+@SuppressWarnings("rawtypes")
+public final class StringToEnumConverterFactory implements ConverterFactory<String, Enum> {
+
+    @SuppressWarnings("unchecked")
+    public <T extends Enum> Converter<String, T> getConverter(Class<T> targetType) {
+        return new StringToEnumConverter(targetType);
+    }
+
+    private final class StringToEnumConverter<T extends Enum> implements Converter<String, T> {
+
+        private Class<T> enumType;
+
+        public StringToEnumConverter(Class<T> enumType) {
+            this.enumType = enumType;
+        }
+
+        @SuppressWarnings("unchecked")
+        public T convert(String source) {
+            if (source == null || source.trim().isEmpty()) {
+                return null;
+            }
+
+            return (T) Enum.valueOf(this.enumType, source.trim().toUpperCase());
+
+        }
+    }
+}
